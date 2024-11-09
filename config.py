0363c8c5e737c194ab175c0be19cdca2e548519e
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2024-11-09 12:08:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY  = '1a52ece65ce46d41b23917a2a98c88bf'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345@localhost/iiestportal'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
