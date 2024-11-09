6d20140e6c7b9cfcb920763b1edfed5ec6a739b4
anne-ananya <2021itb050.ananya@students.iiests.ac.in>
2024-11-09 13:10:36 +0530

    changes to ensure running on local host

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
-
 class Config:
     SECRET_KEY  = '1a52ece65ce46d41b23917a2a98c88bf'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345@localhost/iiestportal'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
 # Ensure the folder exists
 os.makedirs(Config.UPLOAD_FOLDER, exist_ok=True)
