7802cea924d2ce80fcbe16ec4ffbe230a88d7c5c
anne-ananya <2021itb050.ananya@students.iiests.ac.in>
2024-11-09 22:53:11 +0530

    first commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 class Config:
-    SECRET_KEY  = '1a52ece65ce46d41b23917a2a98c88bf'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345@localhost/iiestportal'
+    SECRET_KEY  = os.getenv('SECRET_KEY', os.urandom(24))
+    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL', 'postgres_url_here')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
